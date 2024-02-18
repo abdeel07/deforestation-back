299afcc561b5903a1e0285cfceef01986bba3b0c
AbdeelBrh <abdelilah.bourhou@gmail.com>
2024-02-18 22:17:56 +0100

    init commit

---
+++ src/main/java/com/app/deforestationapp/DeforestationAppApplication.java
@@ -0,0 +1,13 @@
+package com.app.deforestationapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeforestationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeforestationAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/deforestationapp/DeforestationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.deforestationapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeforestationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
